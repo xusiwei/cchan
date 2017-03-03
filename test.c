ee021b6d8466ae6f24d5dfc3b8dcbf664bbc744d
Siwei Xu <xusiwei1236@163.com>
2017-03-03 21:33:19 +0800

    update test.c

--- test.c
+++ test.c
@@ -13,11 +13,13 @@
 int nbuffers = 10;
 int nproducts = 20;
 int nproducers = 1;
-int nconsumers = 1;
+int produce_cost = 0;
 
 long gettid() { return syscall(SYS_gettid); }
 
 #define INFO(fmt, ...) printf("[%ld] " fmt "\n", gettid(), ##__VA_ARGS__)
+
+#define msleep(ms) usleep((ms)*1000)
 
 void* produce(void* args)
 {
@@ -27,6 +29,7 @@ void* produce(void* args)
 
 	for (i = 0; i < nproducts; i++) {
 		sprintf(line, "item-%d", i);
+		msleep(produce_cost);
 		INFO("produced  %s", line);
 		chan_put(c, strdup(line));
 	}
@@ -35,14 +38,15 @@ void* produce(void* args)
 
 void* consume(void* args)
 {
-	int i;
+	int i, total;
 	char* item;
 	chan_t* c = (chan_t*)args;
 
-	for (i = 0; i < nproducts; i++) {
+	total = nproducers * nproducts;
+	for (i = 0; i < total; i++) {
 		item = (char*)chan_take(c);
-		INFO("consuming %s ...", item);
 		sleep(1);
+		INFO("consumed %s ...", item);
 	}
 	return NULL;
 }
@@ -51,38 +55,37 @@ int main(int argc, char* argv[])
 {
 	int i;
 	chan_t* chan = NULL;
+	pthread_t consumer;
 	pthread_t* producers = NULL;
-	pthread_t* consumers = NULL;
 
-	if (argc > 1) nconsumers = atoi(argv[1]);
+	if (argc > 1) produce_cost = atoi(argv[1]);
 	if (argc > 2) nproducers = atoi(argv[2]);
 	if (argc > 3) nproducts = atoi(argv[3]);
-	if (argc > 4) nbuffers = atoi(argv[34]);
+	if (argc > 4) nbuffers = atoi(argv[4]);
 
 	chan = chan_new(nbuffers);
 	assert(chan);
 
+	// create producers
 	producers = (pthread_t*)malloc(nproducers * sizeof(pthread_t));
 	assert(producers);
+
+	// start producers
 	for (i = 0; i < nproducers; i++) {
 		pthread_create(&producers[i], NULL, produce, (void*)chan);
 	}
 
-	consumers = (pthread_t*)malloc(nconsumers * sizeof(pthread_t));
-	assert(consumers);
-	for (i = 0; i < nconsumers; i++) {
-		pthread_create(&consumers[i], NULL, consume, (void*)chan);
-	}
+	// start consumer
+	pthread_create(&consumer, NULL, consume, (void*)chan);
 
-	for (i = 0; i < nconsumers; i++) {
-		pthread_join(consumers[i], NULL);
-	}
+	// wait consumer done
+	pthread_join(consumer, NULL);
 
+	// wait producers done
 	for (i = 0; i < nproducers; i++) {
 		pthread_join(producers[i], NULL);
 	}
 
-	free(consumers);
 	free(producers);
 	chan_delete(chan);
 
