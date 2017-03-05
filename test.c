686363f6539126f2901b19fd9a2664d2c45b8c83
Siwei Xu <xusiwei1236@163.com>
2017-03-05 15:08:03 +0800

    add more cases

--- test.c
+++ test.c
@@ -2,22 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include <pthread.h>
 #include <sys/types.h>
 #include <syscall.h>
 #include <unistd.h>
 
+#if (defined __STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+#include <stdatomic.h>
+#else // __STDC_VERSION__
+#define atomic_int int
+#define atomic_fetch_add __sync_fetch_and_add
+#endif // __STDC_VERSION__
+
 #include "cchan.h"
 
 int nbuffers = 10;
 int nproducts = 20;
 int nproducers = 1;
+int nconsumers = 1;
 int produce_cost = 0;
 
 long gettid() { return syscall(SYS_gettid); }
 
-#define INFO(fmt, ...) printf("[%ld] " fmt "\n", gettid(), ##__VA_ARGS__)
+#define INFO(fmt, ...)                                                         \
+	do {                                                                   \
+		struct timespec ts;                                            \
+		char buffer[128];                                              \
+		clock_gettime(CLOCK_REALTIME, &ts);                            \
+		strftime(buffer, sizeof(buffer), "%F %T",                      \
+			 localtime(&ts.tv_sec));                               \
+		printf("[%s.%ld] %ld " fmt "\n", buffer,                       \
+		       ts.tv_nsec / (1000 * 1000), gettid(), ##__VA_ARGS__);   \
+	} while (0)
 
 #define msleep(ms) usleep((ms)*1000)
 
@@ -27,8 +45,10 @@ void* produce(void* args)
 	char line[128];
 	chan_t* c = (chan_t*)args;
 
+	static atomic_int count = 0;
 	for (i = 0; i < nproducts; i++) {
-		sprintf(line, "item-%d", i);
+		int id = atomic_fetch_add(&count, 1);
+		sprintf(line, "item-%d", id);
 		msleep(produce_cost);
 		INFO("produced  %s", line);
 		chan_put(c, strdup(line));
@@ -36,14 +56,14 @@ void* produce(void* args)
 	return NULL;
 }
 
+static atomic_int rest = 0;
+
 void* consume(void* args)
 {
-	int i, total;
 	char* item;
 	chan_t* c = (chan_t*)args;
 
-	total = nproducers * nproducts;
-	for (i = 0; i < total; i++) {
+	while (atomic_fetch_add(&rest, -1) >= 1) {
 		item = (char*)chan_take(c);
 		sleep(1);
 		INFO("consumed %s ...", item);
@@ -56,16 +76,27 @@ int main(int argc, char* argv[])
 {
 	int i;
 	chan_t* chan = NULL;
-	pthread_t consumer;
+	pthread_t* consumers = NULL;
 	pthread_t* producers = NULL;
 
-	if (argc > 1) produce_cost = atoi(argv[1]);
-	if (argc > 2) nproducers = atoi(argv[2]);
-	if (argc > 3) nproducts = atoi(argv[3]);
-	if (argc > 4) nbuffers = atoi(argv[4]);
+	int c = 0;
+	if (argc > ++c) produce_cost = atoi(argv[c]);
+	if (argc > ++c) nproducers = atoi(argv[c]);
+	if (argc > ++c) nconsumers = atoi(argv[c]);
+	if (argc > ++c) nproducts = atoi(argv[c]);
+	if (argc > ++c) nbuffers = atoi(argv[c]);
+
+#ifdef __STDC_VERSION__
+	printf("__STDC_VERSION__: %ld\n", __STDC_VERSION__);
+#endif
+	atomic_fetch_add(&rest, nproducers * nproducts);
 
 	chan = chan_new(nbuffers);
 	assert(chan);
+
+	// create producers
+	consumers = (pthread_t*)malloc(nconsumers * sizeof(pthread_t));
+	assert(consumers);
 
 	// create producers
 	producers = (pthread_t*)malloc(nproducers * sizeof(pthread_t));
@@ -76,17 +107,22 @@ int main(int argc, char* argv[])
 		pthread_create(&producers[i], NULL, produce, (void*)chan);
 	}
 
-	// start consumer
-	pthread_create(&consumer, NULL, consume, (void*)chan);
+	// start consumers
+	for (i = 0; i < nconsumers; i++) {
+		pthread_create(&consumers[i], NULL, consume, (void*)chan);
+	}
 
-	// wait consumer done
-	pthread_join(consumer, NULL);
+	// wait consumers done
+	for (i = 0; i < nconsumers; i++) {
+		pthread_join(consumers[i], NULL);
+	}
 
 	// wait producers done
 	for (i = 0; i < nproducers; i++) {
 		pthread_join(producers[i], NULL);
 	}
 
+	free(consumers);
 	free(producers);
 	chan_delete(chan);
 
